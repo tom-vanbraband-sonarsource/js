84b47b41901d8759f3f12bf2fc6dd010747f43fe
Tom <tom.vanbraband@sonarsource.com>
2025-09-10 11:04:10 +0200

    Refactor rendering logic for row data in dummy-file.js

--- dummy-file.js
+++ dummy-file.js
@@ -1,1 +1,25 @@
-;
+if (typeof (row) === 'string') {
+      return (
+        <Grid key={i}>
+          <Divider className={classes.divider} />
+            <Typography>
+              { row }
+            </Typography>
+        </Grid>
+      )
+    }
+
+    if (row.constructor === Array) {
+      return row.map((item, index) => (
+        <Grid key={index}>
+          { this.getItem(item, formProps) }
+        </Grid>
+      ))
+    }
+
+    return (
+      <Grid key={i}>
+        { this.getItem(row, formProps) }
+      </Grid>
+    )
+  };
