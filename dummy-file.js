6e94f008303dd0d063a81ab820a2cd4a2092c4d2
Tom <tom.vanbraband@sonarsource.com>
2025-09-10 11:05:51 +0200

    Refactor price calculation logic in getPrice function

--- dummy-file.js
+++ dummy-file.js
@@ -1,25 +1,21 @@
-if (typeof (row) === 'string') {
-      return (
-        <Grid key={i}>
-          <Divider className={classes.divider} />
-            <Typography>
-              { row }
-            </Typography>
-        </Grid>
-      )
-    }
+function getPrice(user, item, isSpecialOffer) {
+  let price = item.basePrice;
 
-    if (row.constructor === Array) {
-      return row.map((item, index) => (
-        <Grid key={index}>
-          { this.getItem(item, formProps) }
-        </Grid>
-      ))
+  if (user.isLoggedIn) { // +1
+    if (user.level === 'gold') { // +2
+      price *= 0.8; // 20% discount for gold members
+      if (isSpecialOffer) { // +3
+        if (item.category === 'electronics') { // +4
+          price -= 10; // Extra $10 off on special offer electronics
+        }
+      }
+    } else if (user.level === 'silver') { // +1
+      price *= 0.9; // 10% discount for silver members
     }
-
-    return (
-      <Grid key={i}>
-        { this.getItem(row, formProps) }
-      </Grid>
-    )
-  };
+  } else {
+    if (isSpecialOffer) { // +2
+      price *= 0.95; // 5% discount for guests on special offers
+    }
+  }
+  return price;
+}
